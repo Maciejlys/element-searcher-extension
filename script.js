97963027e5d2a3bd0e20e4a35d40d3c70c2ec8e3
Bartłomiej <Bartekkur1@gmail.com>
2023-06-22 16:39:01 +0200

    Script code quality improvement (#1)
    * script code quality improvement
    
    * Added suggested improvements

--- script.js
+++ script.js
@@ -1,76 +1,92 @@
-const input = document.getElementById("input");
-const button = document.getElementById("button");
+const EventKey = {
+  ENTER: 'enter'
+};
+
+const ColorList = [
+  "hotpink",
+  "deepPink",
+  "MediumVioletRed",
+  "PaleVioletRed",
+  "Coral",
+  "Tomato",
+  "Orange",
+  "Chocolate",
+  "Peru",
+  "Sienna",
+  "SaddleBrown",
+  "Maroon",
+  "Crimson",
+];
+
+const attachEventListeners = () => {
+  const inputElement = document.getElementById("input");
+  const buttonElement = document.getElementById("button");
+
+  buttonElement.addEventListener("click", () => handleFormSubmit(inputElement));
+  inputElement.addEventListener("keypress", event => event.key === EventKey.ENTER && buttonElement.click());
+  inputElement.addEventListener("change", () => {
+    chrome.storage.sync.set({ input: inputElement.value });
+  });
+};
 
 document.addEventListener("DOMContentLoaded", function () {
-  button.addEventListener("click", callback);
-  input.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      button.click();
-    }
-  });
-  input.addEventListener("change", () => {
-    chrome.storage.sync.set({ input: input.value });
-  });
-  chrome.storage.sync.get(["input"], function (result) {
+  attachEventListeners();
+
+  chrome.storage.sync.get(["input"], result => {
     input.value = result.input || "";
   });
 });
 
-async function callback() {
+async function handleFormSubmit(input) {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const selectors = input.value.split(" ");
 
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: (selectors) => {
-      colors = [
-        "hotpink",
-        "deepPink",
-        "MediumVioletRed",
-        "PaleVioletRed",
-        "Coral",
-        "Tomato",
-        "Orange",
-        "Chocolate",
-        "Peru",
-        "Sienna",
-        "SaddleBrown",
-        "Maroon",
-        "Crimson",
-      ];
-      roots = [document.documentElement];
-      if (document.querySelector("iframe")) {
-        roots.push(document.querySelector("iframe").contentWindow.document.documentElement);
-      }
-      roots.forEach((root) => {
-        selectors.forEach((selector) => {
-          try {
-            const randomColor = colors[Math.floor(Math.random() * colors.length)];
-            root.querySelectorAll(selector).forEach((element) => {
-              element.style.outline = `3px ${randomColor} solid`;
-              element.style.position = "relative";
-              if (element.firstChild.style) {
-                element.firstChild.style.outline = `3px ${randomColor} solid`;
-                element.firstChild.style.position = "relative";
-              }
-              const tag = document.createElement("span");
-              tag.innerText = selector;
-              tag.style.color = "white";
-              tag.style.position = "absolute";
-              tag.style.fontSize = "14px";
-              tag.style.top = "-17px";
-              tag.style.left = "-3px";
-              tag.style.padding = "0 8px";
-              tag.style.lineHeight = "1";
-              tag.style.backgroundColor = randomColor;
-              element.appendChild(tag);
-            });
-          } catch (e) {
-            console.log("No components on this page!", selectors, e);
-          }
-        });
-      });
-    },
-    args: [selectors],
+    func: backgroundScript,
+    args: [selectors, ColorList]
   });
 }
+
+const backgroundScript = (selectors, colors) => {
+  const injectElementStyle = (element, randomColor) => {
+    element.style.outline = `3px ${randomColor} solid`;
+    element.style.position = "relative";
+    if (element.firstChild.style) {
+      element.firstChild.style.outline = `3px ${randomColor} solid`;
+      element.firstChild.style.position = "relative";
+    }
+  };
+
+  const injectSpanElement = (element, randomColor, selector) => {
+    const tag = document.createElement("span");
+    tag.innerText = selector;
+    tag.style.color = "white";
+    tag.style.position = "absolute";
+    tag.style.fontSize = "14px";
+    tag.style.top = "-17px";
+    tag.style.left = "-3px";
+    tag.style.padding = "0 8px";
+    tag.style.lineHeight = "1";
+    tag.style.backgroundColor = randomColor;
+    element.appendChild(tag);
+  };
+
+  const roots = [document.documentElement];
+  if (document.querySelector("iframe")) {
+    roots.push(document.querySelector("iframe").contentWindow.document.documentElement);
+  }
+  roots.forEach((root) => {
+    selectors.forEach((selector) => {
+      try {
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        root.querySelectorAll(selector).forEach((element) => {
+          injectElementStyle(element, randomColor);
+          injectSpanElement(element, randomColor, selector);
+        });
+      } catch (e) {
+        console.log("No components on this page!", selectors, e);
+      }
+    });
+  });
+};
