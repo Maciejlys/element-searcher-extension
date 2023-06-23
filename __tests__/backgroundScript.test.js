6abacde072fdf22f0bcfc9123624dac18714d3ca
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2023-06-23 16:08:24 +0200

    more test cases

--- __tests__/backgroundScript.test.js
+++ __tests__/backgroundScript.test.js
@@ -1,17 +1,16 @@
-import { JSDOM } from 'jsdom';
+import { JSDOM } from "jsdom";
 import { describe, expect, test, beforeAll } from "vitest";
 import backgroundScript from "../src/backgroundScript";
 
 describe("backgroundScript", () => {
-
   let dom;
-  const colors = ['red'];
-  const selectors = ['h1'];
+  const colors = ["red"];
+  const selectors = ["h1"];
 
   const fakeElementsId = {
-    HEADER: 'header',
-    SUBHEADER: 'subheader',
-    CONTAINER: 'container'
+    HEADER: "header",
+    SUBHEADER: "subheader",
+    CONTAINER: "container",
   };
 
   beforeAll(() => {
@@ -26,12 +25,12 @@ describe("backgroundScript", () => {
   });
 
   test("Should override element style", () => {
-
     // Check if header style is override;
     const headerElement = dom.window.document.getElementById(fakeElementsId.HEADER);
     const headerElementStyle = headerElement.style._values;
     expect(headerElementStyle).toStrictEqual({
-      outline: '3px red solid', position: 'relative'
+      outline: "3px red solid",
+      position: "relative",
     });
 
     // Make sure that subheader is untouched
@@ -40,26 +39,38 @@ describe("backgroundScript", () => {
     expect(subheaderElementStyle).toEqual({});
   });
 
-  test('Should inject a span element', () => {
+  test("Should inject a span element", () => {
     const headerElement = dom.window.document.getElementById(fakeElementsId.HEADER);
-    const injectedSpanElement = headerElement.getElementsByTagName('span')[0];
+    const injectedSpanElement = headerElement.getElementsByTagName("span")[0];
     expect(injectedSpanElement).not.undefined;
 
     // Check injected span styles
     const spanElementStyle = injectedSpanElement.style._values;
     expect(spanElementStyle).toEqual({
-      'color': 'white',
-      'position': 'absolute',
-      'font-size': '14px',
-      'top': '-17px',
-      'left': '-3px',
-      'padding': '0px 8px',
-      'padding-top': '0px',
-      'padding-right': '8px',
-      'padding-bottom': '0px',
-      'padding-left': '8px',
-      'line-height': '1',
-      'background-color': 'red'
+      color: "white",
+      position: "absolute",
+      "font-size": "14px",
+      top: "-17px",
+      left: "-3px",
+      padding: "0px 8px",
+      "padding-top": "0px",
+      "padding-right": "8px",
+      "padding-bottom": "0px",
+      "padding-left": "8px",
+      "line-height": "1",
+      "background-color": "red",
     });
+  });
+
+  test("should set the tag text to the selector", () => {
+    expect(true).toBe(true);
+  });
+
+  test("should override styles in the iframe root", () => {
+    expect(true).toBe(true);
+  });
+
+  test("should log an error if the selector was not found", () => {
+    expect(true).toBe(true);
   });
 });
